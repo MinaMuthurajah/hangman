98f3a5996314ea1bc46eab08c30bcee501e2c5f0
MinaMuthurajah <117191687+MinaMuthurajah@users.noreply.github.com>
2022-11-02 13:47:11 -0300

    Add files via upload

---
+++ milestone_2.py
@@ -0,0 +1,30 @@
+word_list = ['mangoes', 'pineapples', 'peaches' 'oranges', 'strawberries']
+print('word_list')
+python 3 milestone_2.py
+import random
+print(random.choices(word_list))
+word_list
+print(word_list)
+word_list = ['mangoes', 'pineapples', 'peaches' 'oranges', 'strawberries']
+print('word_list')
+print('random.choices(word_list)')
+print(random.choices('word_list'))
+random.choice(word_list)
+word = random.choice(word_list)
+print ('word')
+print('word')
+word = random.choice('word_list')
+print('word')
+print (random.choice('word_list'))
+print(word)
+print('word')
+input ('Please enter a single letter:')
+guess = input ('Please enter a single letter:')
+if len(guess) == 1 and guess.isalpha():
+    print ('good guess!')
+else:
+    print ('Oops! That is not a valid input')
+
+   
+
+  
